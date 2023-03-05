8dfe5f8e319a9e93a60be06197c592871f17248b
WIPITO-2023 <myprojects@domotiquebec.com>
2023-03-05 10:42:34 -0500

    Hice un cambio del codigo

--- tableau.cpp
+++ tableau.cpp
@@ -1,52 +1,25 @@
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
-
 int main(void){
+	int N = 6;
+//	int i=0;
 	
-	int taille ;
-	
-	int t[] = {12, -2, 34, 63, -31, 5};
-			
-	taille = sizeof(t)/sizeof(t[0]);
+	int t[N] = {12, -2, 34, 63, -31, 5};
 	int temp;
-	bool isChange(false);
+	bool ischange(false);
 	
-cout << "Voici les valeurs dans desordre: ";
-			
-	for(int i=0; i< taille; i++) cout << t[i] << "  ";
-		cout <<  endl;
+	cout << "Voici les valeurs dans le desordre: ";
+	
+//	for (int i=0; i < N; i++){
+//		cout << t[i] << "  ";
+//		cout << endl;
+//	}
+	
+	for (int i = 0; i < 6; i++) {
 		
-	//Mettre en ordre les valeurs
-		
-		do {
-			isChange = false;			
-				for(int i=0; i<taille; i++){
-					cout << "Valor de i: " << i<< endl;
-					
-					if(t[i] > t[i+1]){// Compare avec le souvient
-						
-						temp = t[i]; //stocke sa valeur de facon temporel dans la variable temp
-						cout << "Debugging: temp: " << temp << endl;
-					//	system("pause");
-						t[i] = t[i+1];
-						t[i +1] = temp;
-						isChange = true;						
-					}					
-				}			
-		
-			}while(isChange);
-			
-//	cout << "Voici les valeurs dans l'ordre: ";
-	
-	
-	int temporal[6];
-	copy_n(t,6,temporal);
-			
-	for(int i=0; i< taille; i++) cout << temporal[i] << "  ";
-		cout <<  endl;		
-	
-	return 0;
+		//	cout << t[i] << "  ";
+	}
+	 
 }
 
