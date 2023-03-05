e877d1258c49b72aacd0feffbbbd6b4ceab83c76
WIPITO-2023 <myprojects@domotiquebec.com>
2023-03-05 10:54:28 -0500

    Segunda modificacion usando el cout

--- tableau.cpp
+++ tableau.cpp
@@ -2,24 +2,7 @@
 
 using namespace std;
 int main(void){
-	int N = 6;
-//	int i=0;
-	
-	int t[N] = {12, -2, 34, 63, -31, 5};
-	int temp;
-	bool ischange(false);
-	
-	cout << "Voici les valeurs dans le desordre: ";
-	
-//	for (int i=0; i < N; i++){
-//		cout << t[i] << "  ";
-//		cout << endl;
-//	}
-	
-	for (int i = 0; i < 6; i++) {
-		
-		//	cout << t[i] << "  ";
-	}
+ cout << "Probando el codigo " << endl;
 	 
 }
 
